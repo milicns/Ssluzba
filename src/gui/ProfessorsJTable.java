45ea28ab2370ea4f84cc7745a0550132f74f0d55
Vasilije Milic <milic.ra208.2019@uns.ac.rs>
2021-12-14 13:35:21 +0100

    Student table view and tabs implemented

---
+++ src/gui/ProfessorsJTable.java
@@ -0,0 +1,30 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.TableCellRenderer;
+
+public class ProfessorsJTable extends JTable{
+
+	public ProfessorsJTable() {
+		this.setRowSelectionAllowed(true);
+		this.setColumnSelectionAllowed(true);
+		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);	
+	}
+	
+	@Override
+	public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+		Component c = super.prepareRenderer(renderer, row, column);
+		if (isRowSelected(row)) {
+			c.setBackground(Color.LIGHT_GRAY);
+		} else {
+			c.setBackground(Color.WHITE);
+		}
+		return c;
+	}
+	
+	
+}
